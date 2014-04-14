5fa4fee1db46390100e1a25108ad3ba972cee0c6
puhrome <phirom.yim@fullsail.edu>
2014-04-14 13:07:33 -0400

    Conditional If and Else Statement
    Using WebStorm to add if statement for condition. Using else in place
    of if statement to simplify code for conditional statement.

---
+++ js/script.js
@@ -0,0 +1,18 @@
+/**
+ * If and Else Statement Created by Phirom Yim on 4/14/14.
+ */
+
+var kidHeight = 47;
+var minHeight = 48;
+
+//if the child is tall enough, print to the console, you can ride
+//if the kid is over 48 inches in height
+if(kidHeight > minHeight) {
+    //code performed if condition is true
+    //condition goes inside of ( )
+    //code block goes inside of { }
+    console.log("You can ride the coaster");
+}else{
+    //code performed if condition is true
+    console.log("Sorry kid, you've got some growing to do first!");
+}
